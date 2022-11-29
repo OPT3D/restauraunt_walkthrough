1b66086c13ab1f23bea1c167d66007b09db2b1f3
OPT3D <daveketron@gmail.com>
2022-11-28 22:22:42 -0500

    Added a lot of functionality

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+// import {images} from '../../constants'
+import {SubHeading} from '../../components'
+import { images } from '../../constants';
+
 
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new Flavor"/>
+      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin: '2rem 0'}}> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam temporibus, labore maiores quo nulla ex nostrum eos. Sed, alias, nisi magnam veritatis nobis qui ad cumque enim consequatur nostrum ex? </p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt='welcome' />
+    </div>
   </div>
 );
 
